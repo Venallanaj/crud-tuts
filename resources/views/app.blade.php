143e16657d47efc16803ee3b0879f32d46b114a7
Frontenddev123 <vena.llanaj@outlook.com>
2022-10-19 23:14:06 +0200

    first commit

---
+++ resources/views/app.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Laravel VUE CRUD Application - LaravelTuts</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" crossorigin="anonymous">
+    @vite(['resources/scss/app.scss', 'resources/css/app.css', 'resources/js/app.js'])</head>
+<body>
+    <div id="app"> <h1>test</h1></div>
+
+    @vite(['resources/scss/app.scss', 'resources/css/app.css', 'resources/js/app.js'])
+
+   
+</body>
+</html>
